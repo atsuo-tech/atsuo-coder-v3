aa0043767a61bcd91b4cd187680020f4d0e88ed1
yama.can <yama.can95@gmail.com>
2025-11-24 06:45:18 +0900

    [add] Add appropriate condition to sitemap

--- src/app/contests/tasks-sitemap.xml/route.ts
+++ src/app/contests/tasks-sitemap.xml/route.ts
@@ -3,7 +3,7 @@ import { sitemapContestCache } from "../sitemap";
 
 export async function GET() {
 
-	const contests = await sitemapContestCache();
+	const contests = (await sitemapContestCache()).filter(contest => contest.end_time <= new Date());
 
 	const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
