fdabde943067e9ef2c5f30b3d88eb277b771eb2c
yama.can <yama.can95@gmail.com>
2025-12-12 01:30:20 +0900

    [fix] Fix sitemap generation

---
+++ src/app/contests/sitemap.xml/route.ts
@@ -0,0 +1,23 @@
+import { getPublicContests } from "@/lib/contest";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+	const contests = await getPublicContests();
+
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${contests.map((contest) => `
+<url>
+	<loc>https://judge.w-pcp.dev/contests/${contest.url_id}</loc>
+	<changefreq>weekly</changefreq>
+	<priority>0.7</priority>
+</url>
+`).join('')}
+    </urlset>
+  `;
+  
+	return new NextResponse(xml, {
+		headers: { "Content-Type": "application/xml" },
+	});
+}
